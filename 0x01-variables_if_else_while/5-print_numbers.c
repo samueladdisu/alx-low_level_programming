3ab0eb7187e99a4a654b73089a1851d754c46547
samueladdisu <samueladdisu7@gmail.com>
2022-03-11 18:39:43 +0300

    num

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: numbers 
+ * Description: numbers
+ *
  * Return: 0
  */
 int main(void)
 {
 	int n = 10;
 	int i = 0;
-	
-	while(i < n)
+
+	while (i < n)
 	{
 		printf("%i", i);
 		i++;
 	}
+
 	printf("\n");
+
 	return (0);
 }
