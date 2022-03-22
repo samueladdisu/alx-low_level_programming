eaa06193ba54707d7df8e1eb9626c26589fb6d43
samueladdisu <samueladdisu7@gmail.com>
2022-03-22 18:39:07 +0300

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string followed by new line
+ * @str: contains the string
+ *
+ * Returns: noting
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
