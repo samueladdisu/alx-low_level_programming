50f141da75f1ba065ca83efd3321ac02cc93ee13
samueladdisu <samueladdisu7@gmail.com>
2022-03-22 18:59:44 +0300

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - print reverse of given string
+ * @str: contains string
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	char ch;
+	int i,j;
+	for ( i = 0; s[i] != 0; i++)
+		;
+	
+	for ( j = i - 1; j >= 0; j--)
+	{
+		ch = s[j];
+		printf("%c", ch);
+	}
+	printf("\n");
+}
