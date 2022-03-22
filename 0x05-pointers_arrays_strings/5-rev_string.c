813f93f49005f7289b4f8c3c499c5026a8407afe
samueladdisu <samueladdisu7@gmail.com>
2022-03-22 19:45:24 +0300

    local

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * rev_string - revesre a given string
+ * @s: contains string
+ * Returns: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i;
+	char ch[];
+
+	for(i = 10; i >= 0; i--)
+	{
+		ch[] += s[i];
+	}
+	s[10] = ch[];
+}
