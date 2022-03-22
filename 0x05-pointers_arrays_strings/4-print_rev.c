32971a53035daae97d285fa5048eef61ed8904f6
samueladdisu <samueladdisu7@gmail.com>
2022-03-22 19:05:38 +0300

    betty fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 /**
  * print_rev - print reverse of given string
- * @str: contains string
+ * @s: contains string
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
 	char ch;
-	int i,j;
-	for ( i = 0; s[i] != 0; i++)
+	int i, j;
+
+	for (i = 0; s[i] != 0; i++)
 		;
-	
-	for ( j = i - 1; j >= 0; j--)
+	for (j = i - 1; j >= 0; j--)
 	{
 		ch = s[j];
 		printf("%c", ch);
